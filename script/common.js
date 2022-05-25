2c4c34e1642ea83aea67ad3168b9afa149d79af4
everything <75941260+EverythingL@users.noreply.github.com>
2022-05-25 17:01:50 +0300

    add mobile tabs version + mobile menu

--- script/common.js
+++ script/common.js
@@ -2,25 +2,24 @@ document.addEventListener('DOMContentLoaded', (e) => {
     const $$ = (selector) => document.querySelector(selector);
     const tabList = document.querySelectorAll('.tab-link');
     const tabContentList = document.querySelectorAll('.tabs-content');
-
-
-
-    tabList.forEach(btn => {
-        btn.addEventListener('click', (e) => {
-            tabList.forEach(btn => {
-                btn.classList.remove('tab-link-active');
-            })
-            tabContentList.forEach(content => {
-                content.classList.add('tabs-content-hidden');
-            })
-            e.target.classList.add('tab-link-active');
-            const dataTab = e.target.dataset.tab;
-            tabContentList[dataTab].classList.remove('tabs-content-hidden');
-        })
-    })
-
     const faqItemList = document.querySelectorAll('.faq-item-visible');
 
+
+    if (window.outerWidth > 810) {
+        tabList.forEach(btn => {
+            btn.addEventListener('click', (e) => {
+                tabList.forEach(btn => {
+                    btn.classList.remove('tab-link-active');
+                })
+                tabContentList.forEach(content => {
+                    content.classList.add('tabs-content-hidden');
+                })
+                e.target.classList.add('tab-link-active');
+                const dataTab = e.target.dataset.tab;
+                tabContentList[dataTab].classList.remove('tabs-content-hidden');
+            })
+        })
+    }
 
     faqItemList.forEach(faq => {
         faq.addEventListener('click', (e) => {
@@ -31,11 +30,27 @@ document.addEventListener('DOMContentLoaded', (e) => {
                 })
             }
             e.target.closest('li').classList.toggle('faq-item-hide');
-
         })
     })
 
-    console.log(faqItemList);
 
+    if (window.outerWidth < 810) {
+
+        $('.tabs-content-wrapper').slick({
+            dots: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1.02,
+            // centerMode: true,
+            // variableWidth: true
+        });
+
+
+        $$('.header-nav_burger').addEventListener('click', (e) => {
+            $$('.header-nav_list').classList.toggle('open-menu')
+            e.target.closest('button').classList.toggle('burger-open')
+        })
+
+    }
 
 })
